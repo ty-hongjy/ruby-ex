02dee90e31e9ae6c51b4706eeb785f3adb5a3eb2
lrj <leirenjie@163.com>
2018-02-20 12:25:46 +0800

    20180220

--- ex47/lib/ex47/game.rb
+++ ex47/lib/ex47/game.rb
@@ -1,20 +1,20 @@
 class Room
-	def initialize(name,description)
-		@name = name
-		@description = description
-		@path = ()
-	end
-
 	attr_reader :name
 	attr_reader :paths
 	attr_reader :description
 
+	def initialize(name,description)
+		@name = name
+		@description = description
+		@paths = {}
+	end
+
 	def go(direction)
-		return @paths(direction)
+		return @paths[direction]
 	end
 
 	def add_paths(paths)
-		@path.update(paths)
+		@paths.update(paths)
 	end
 
 end
