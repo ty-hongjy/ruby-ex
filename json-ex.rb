4ba5a2f049bb65771ea816f24a827907460687d5
ty-hongjy <hongjy1@163.com>
2018-06-12 17:00:31 +0800

    20180612

---
+++ json-ex.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'pp'
+
+s='[{"factory_code":"601902","area_code":"035008"}]'
+
+
+#json = File.read('input.json')
+json = File.read('json.txt')
+obj = JSON.parse(json)  #转为hash  这里的json是字符串不是json对象
+
+pp obj
+
+
+#json = File.read('json.txt')
+obj = JSON.parse(s)  #转为hash  这里的json是字符串不是json对象
+pp obj
